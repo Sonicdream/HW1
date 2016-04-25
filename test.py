4d6ae6cced56c9db24a109f940a56e484fa7f60b
Sonicdream <joker17993@gmail.com>
2016-04-25 02:43:55 -0700

    just try2

---
+++ test.py
@@ -0,0 +1,3 @@
+PythonWin 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32.
+Portions Copyright 1994-2008 Mark Hammond - see 'Help/About PythonWin' for further copyright information.
+>>> print ("test")
